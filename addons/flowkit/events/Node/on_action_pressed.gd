3096c946d5086d677330740fb9ca8ea74fe67521
Dum <95107189+DumsStuff@users.noreply.github.com>
2025-11-30 16:31:06 -0500

    Create on_action_pressed.gd

---
+++ addons/flowkit/events/Node/on_action_pressed.gd
@@ -0,0 +1,24 @@
+extends FKAction
+
+func get_description() -> String:
+	return "This event occurs when an Action from the input map is pressed."
+
+func get_id() -> String:
+    return "on_action_pressed"
+
+func get_name() -> String:
+    return "On Action Pressed"
+
+func get_supported_types() -> Array:
+    return ["Node2D"]
+
+func get_inputs() -> Array:
+    return [
+        {"name": "amount", "type": "float"},
+        {"name": "message", "type": "String"}
+    ]
+
+func execute(node: Node, inputs: Dictionary) -> void:
+    var amount = inputs.get("amount", 0.0)
+    var message = inputs.get("message", "")
+    print(message, " - ", amount)
