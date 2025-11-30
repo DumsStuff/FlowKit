aabfd7fdbc5944020464a56c4847dffde9fa47e0
Dum <95107189+DumsStuff@users.noreply.github.com>
2025-11-30 16:49:41 -0500

    Update on_action_pressed.gd

--- addons/flowkit/events/Node/on_action_pressed.gd
+++ addons/flowkit/events/Node/on_action_pressed.gd
@@ -1,24 +1,38 @@
-extends FKAction
+extends FKEvent
 
 func get_description() -> String:
-	return "This event occurs when an Action from the input map is pressed."
+	return "This event occurs when an action from the InputMap is pressed."
 
 func get_id() -> String:
-    return "on_action_pressed"
+	return "on_action_pressed"
 
 func get_name() -> String:
-    return "On Action Pressed"
+	return "On Action Pressed"
 
-func get_supported_types() -> Array:
-    return ["Node2D"]
+func get_supported_types() -> Array[String]:
+	return ["Node", "System"]
 
 func get_inputs() -> Array:
-    return [
-        {"name": "amount", "type": "float"},
-        {"name": "message", "type": "String"}
-    ]
+	return [
+		{
+			"name": "action_name",
+			"type": "String",
+			"default": "",
+			"hint": "The name of the action from InputMap (e.g., 'ui_accept', 'jump')"
+		}
+	]
 
-func execute(node: Node, inputs: Dictionary) -> void:
-    var amount = inputs.get("amount", 0.0)
-    var message = inputs.get("message", "")
-    print(message, " - ", amount)
+func poll(node: Node, inputs: Dictionary = {}) -> bool:
+	if not node or not node.is_inside_tree():
+		return false
+	
+	var action_name = inputs.get("action_name", "")
+	
+	if action_name.is_empty():
+		return false
+	
+	if not InputMap.has_action(action_name):
+		push_warning("Action '%s' not found in InputMap" % action_name)
+		return false
+	
+	return Input.is_action_just_pressed(action_name)
